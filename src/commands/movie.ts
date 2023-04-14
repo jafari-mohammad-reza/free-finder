990770572e10b2662c2649aecb81df5fa9ff644f
Mohammadreza Jafari <mohammadrezajafari.dev@gmail.com>
2023-04-14 20:49:21 +0330

    Optimize movie searcher command.

--- src/commands/movie.ts
+++ src/commands/movie.ts
@@ -1,25 +1,33 @@
 import cheerio from 'cheerio';
 import axios from "axios";
+const parseResults = ($, baseUrl) => {
+    return Array.from($(".flw-item"), (element) => {
+        const titleContainer = $(element).find(".film-detail");
+        const foundTitle = $(titleContainer).find('h2').text().trim();
+        const posterContainer = $(element).find(".film-poster");
+        const url = baseUrl + $(posterContainer).find("a.film-poster-ahref").attr('href');
+        return { title: foundTitle, url };
+    });
+};
 
-import {writeFileSync} from "fs";
-export async function searchForMovie(title:string,limit:number){
+export async function searchForMovie(title: string, limit: number) {
     try {
-        const baseUrl = 'https://movie4kto.net';
-        const response = await  axios.get(`${baseUrl}/search/${title.replaceAll(" " ,"-")}`)
-        const $ =  await cheerio.load(response.data);
-        const results = [];
-        $(".flw-item").each((_, element) => {
-            const titleContainer = $(element).find(".film-detail");
-            const foundTitle = $(titleContainer).find('h2').text().trim();
-            const posterContainer = $(element).find(".film-poster");
-            const url = baseUrl + $(posterContainer).find("a.film-poster-ahref").attr('href');
-            // console.log()
-             results.push({ title: foundTitle, url });
+        const encodedTitle = title.replaceAll(" ", "-");
+        let baseUrl = 'https://movie4kto.net';
+        const fallbackUrl = `https://fmoviesto.cc/search/${encodedTitle}`;
+
+        const response = await axios.get(`${baseUrl}/search/${encodedTitle}`)
+            .catch(async () => await axios.get(fallbackUrl));
+
+        console.log(response.request.responseURL)
+        baseUrl = response.request.host === "fmoviesto.cc" ? 'https://fmoviesto.cc' : baseUrl;
+        const $ = await cheerio.load(response.data);
+        console.log('Result is:')
+        parseResults($, baseUrl).slice(0, limit).forEach(content => {
+            const {url,title} = content
+            console.log(`${title} - ${url} \n`)
         });
-        console.log('Search Results:');
-        results?.slice(0 , limit).forEach((result, index) => {
-            console.log(`${result.title} - ${result.url}`);
-        });
+
     } catch (error) {
         console.error('Error fetching search results');
         return [];
--- src/commands/series.ts
+++
@@ -1,1 +0,0 @@
-export function searchForSeries(title:string){}
